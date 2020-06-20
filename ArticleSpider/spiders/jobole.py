375d3a712aef35eb765b580a2489efd56b5e39fa
suyongzhi <1125699801@qq.com>
2020-06-20 16:40:00 +0800

    提取新闻标题，发布时间，文章正文

--- ArticleSpider/spiders/jobole.py
+++ ArticleSpider/spiders/jobole.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request
 
 
 class JoboleSpider(scrapy.Spider):
     name = 'jobole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/kaifadou/snews-getajax.php?next=%d' % i for i in range(1, 3)]
 
     def parse(self, response):
-        pass
+        article_urls = response.xpath('//a/@href').extract()
+        for article_url in article_urls:
+            article_url = 'http://blog.jobbole.com' + article_url
+            yield Request(url=article_url, callback=self.detail_parse)
+
+    def detail_parse(self, response):
+        title = response.xpath('//div[@class="main_left"]//h2/text()').extract_first()  # 新闻标题
+        create_date = response.xpath('//div[@class="meta"]/span/text()').extract_first()  # 发布时间
+        content = response.xpath('//div[@class="wen_article"]').extract_first()  # 文章正文
